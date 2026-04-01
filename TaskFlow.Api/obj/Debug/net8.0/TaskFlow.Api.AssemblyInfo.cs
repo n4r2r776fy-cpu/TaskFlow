4bc26062a6363cdaa023b66405f290ee32afab03
andron2304 <andriyopanasiuk2008@gmail.com>
2026-04-01 21:58:08 +0300

    aaaaaa

--- TaskFlow.Api/obj/Debug/net8.0/TaskFlow.Api.AssemblyInfo.cs
+++ TaskFlow.Api/obj/Debug/net8.0/TaskFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1b9d6230762077ebeb40e2dee7634fe4e5cc412")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae4bc1cad3de1819241cc641292a3fea7959c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
