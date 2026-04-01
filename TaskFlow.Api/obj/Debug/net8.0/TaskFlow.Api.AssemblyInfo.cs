f0d742a1793a33dc9dc1e088564a754325fd9354
DaDoVa-007 <n4r2r776fy@privaterelay.appleid.com>
2026-04-02 00:54:53 +0300

    aaa

--- TaskFlow.Api/obj/Debug/net8.0/TaskFlow.Api.AssemblyInfo.cs
+++ TaskFlow.Api/obj/Debug/net8.0/TaskFlow.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d50e5650b3e62bc425ee5592b135f0e556e6da3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2644a9f3fdd13d907e7d9778bb42508319cc8822")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskFlow.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
